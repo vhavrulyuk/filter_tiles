a4238fae1979ccbe51004b62bcbb57fe9e609eec
vhavrulyuk <vhavrulyuk@gmail.com>
2017-06-25 03:14:27 +0300

    Refactoring.

--- js/ex_filter_tiles.js
+++ js/ex_filter_tiles.js
@@ -3,10 +3,11 @@
         $("ul.categories a").on('click', filterTiles.filterTiles);
     });
 
-    var filterTiles = {
+    var filterTiles;
+    filterTiles = {
 
         filterTiles: function () {
-            selectedGroup = $(this).attr("href").toString().substr(1);
+            selectedGroup = $(this).attr("href").substr(1);
             if (selectedGroup === "cat-all") {
                 $("ul.tiles li").css("display", "inline-block");
             } else {
@@ -17,7 +18,7 @@
             console.log('filtering...' + selectedGroup);
         }
 
-    }
+    };
 })(jQuery);
 
 
