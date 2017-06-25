cadd4474f7a892d57cc1d0b1663c5c1f953c963c
vhavrulyuk <vhavrulyuk@gmail.com>
2017-06-25 04:29:32 +0300

    Refactoring.

--- js/ex_filter_tiles.js
+++ js/ex_filter_tiles.js
@@ -5,19 +5,22 @@
 
     var filterTiles;
     filterTiles = {
+        selectedGroup: "",
 
         filterTiles: function () {
             selectedGroup = $(this).attr("href").substr(1);
+            $("ul.categories a").css("text-shadow", "");
+            $(this).css("text-shadow", "2px 2px 5px blue");
             if (selectedGroup === "cat-all") {
-                $("ul.tiles li").css("display", "inline-block");
+                /*$("ul.tiles li").css("display", "inline-block");*/
+                $("ul.tiles li").slideDown(800);
             } else {
-
-                $("ul.tiles li").css("display", "none");
-                $("li." + selectedGroup).css("display", "inline-block");
+                $("ul.tiles li").slideUp(600);
+                $("li." + selectedGroup).slideDown(800);
+                /* $("ul.tiles li").css("display", "none");
+                 $("li." + selectedGroup).css("display", "inline-block");*/
             }
-            console.log('filtering...' + selectedGroup);
         }
-
     };
 })(jQuery);
 
